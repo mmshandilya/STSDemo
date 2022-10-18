4cb1e9ebc3c9ac9fdb381bc651426bbe08f506d8
Mahesh <Mahesh@LAPTOP-HOBV0367>
2022-10-18 09:50:06 +0530

    New commit

--- MySqlDemo/src/main/java/com/mysql/demo/repository/BookRepo.java
+++ MySqlDemo/src/main/java/com/mysql/demo/repository/BookRepo.java
@@ -1,9 +1,11 @@
 package com.mysql.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mysql.demo.Model.Book;
 
 public interface BookRepo extends JpaRepository<Book, Long>{
-
+	public List<Book> findByBookName(String name);
 }
