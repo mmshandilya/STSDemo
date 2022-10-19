301bb87fe8f03d0a61d1a5bd9f965cb64bbd1c1d
Mahesh <Mahesh@LAPTOP-HOBV0367>
2022-10-19 09:50:42 +0530

    19 Oct

--- MySqlDemo/src/main/java/com/mysql/demo/Controller/BookController.java
+++ MySqlDemo/src/main/java/com/mysql/demo/Controller/BookController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +50,12 @@ public class BookController {
 	@GetMapping("/getBookById/{bookid}")
 	public Optional<Book> getBookById(@PathVariable("bookid") Long bookid) {
 		return bookrepo.findById(bookid);
+	}
+	@DeleteMapping("/deleteBook/{bookid}")
+	public String deleteBookById(@PathVariable("bookid") Long bookid)
+	{
+		bookrepo.deleteById(bookid);
+		logger.warn("Delete from Logback {}", bookid);
+		return "Book Delete Successfully !";
 	}
 }
